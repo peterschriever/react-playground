dd91d4d299b2364aebcd46305234a9a9a78cc8d5
Peter Schriever <peterzen72@gmail.com>
2017-05-06 09:14:06 +0200

    clean up for section 5

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 
-import BookList from '../containers/book_list';
-import BookDetail from '../containers/book_detail';
-
 export default class App extends Component {
   render() {
     return (
       <div>
-          <BookList />
-          <BookDetail />
+          Hello world!
       </div>
     );
   }
